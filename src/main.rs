8c4ff4425b959768b3b90d145a87563f843ba7c6
Akash Bhave <akashbbhave@gmail.com>
2020-04-14 15:24:19 -0400

    Install MPD client, connect to Unix socket

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+extern crate mpd;
+
+use std::os::unix::net::UnixStream;
+use std::io::prelude::*;
+
+use mpd::Client;
+
+pub fn main() {
+    let mut stream = UnixStream::connect("/home/akash/.config/mpd/socket").unwrap();
+    let mut c = Client::new(stream).unwrap();
+    println!("{}", c.music_directory().unwrap());
 }
